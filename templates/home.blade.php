bdbf129e20ad05462bc0b01e01bdca016439980c
Tony LAURENT <contact@tony-laurent.com>
2018-05-14 13:03:14 +0200

    Remove home pagination

--- templates/home.blade.php
+++ templates/home.blade.php
@@ -5,11 +5,4 @@
 
 @section('content')
     @each('theme::post-block', $posts, 'post')
-
-    <nav>
-        <ul class="pager">
-          <li><a href="#">Previous</a></li>
-          <li><a href="#">Next</a></li>
-        </ul>
-    </nav>
 @endsection
